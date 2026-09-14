30e075e86282fb35480c1e1cc02f2718841306bf
Pritam Halder <pritamhalder351@gmail.com>
2026-09-14 14:21:46 +0530

    after login check /me, create extra secure auth middleware before go to controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -98,7 +98,32 @@ const loginUser = async (req, res) => {
     }
 };
 
+const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId)
+            .select("-password");
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        return res.status(200).json({
+            user
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Server error"
+        });
+    }
+};
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 };
